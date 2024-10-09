ebe4580286c0c7636f1d359772cadca4972ada87
ItsMeSamG <itsmesamg123@gmail.com>
2024-10-09 19:02:44 -0400

    hows ur sister

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <main className="container mx-auto py-6">
+        <Button>Our cool ass button</Button>
+      </main>
+    </>
+  );
+}
