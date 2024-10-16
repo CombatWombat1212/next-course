c5e442f85e337dcf15416f46e3b0724b79b325d2
ItsMeSamG <itsmesamg123@gmail.com>
2024-10-16 15:46:45 -0400

    course 3

--- Backend/types/generated/components.d.ts
+++ Backend/types/generated/components.d.ts
@@ -1,5 +1,20 @@
 import type { Struct, Schema } from '@strapi/strapi';
 
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    icon: 'house';
+    description: '';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    subHeading: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images' | 'files'>;
+    link: Schema.Attribute.Component<'components.link', false>;
+  };
+}
+
 export interface SharedSlider extends Struct.ComponentSchema {
   collectionName: 'components_shared_sliders';
   info: {
@@ -62,14 +77,28 @@ export interface SharedMedia extends Struct.ComponentSchema {
   };
 }
 
+export interface ComponentsLink extends Struct.ComponentSchema {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    url: Schema.Attribute.String;
+    text: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'layout.hero-section': LayoutHeroSection;
       'shared.slider': SharedSlider;
       'shared.seo': SharedSeo;
       'shared.rich-text': SharedRichText;
       'shared.quote': SharedQuote;
       'shared.media': SharedMedia;
+      'components.link': ComponentsLink;
     }
   }
 }
--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,10 +1,49 @@
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/custom/HeroSection";
+import qs from "qs";
 
-export default function Home() {
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"],
+            },
+            link: {
+              populate: true,
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  try {
+    const response = await fetch(url.href, {cache: 'no-store'});
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  const { title, description, blocks } = strapiData.data;
+  console.dir(strapiData, { depth: null });
+
   return (
     <>
-      <main className="container mx-auto py-6">
-        <Button>Our cool ass button</Button>
+      <main>
+        <HeroSection data={blocks[0]} />
       </main>
     </>
   );
